48e38cdad2e05893c0462539f379f814d2778f01
Colin Geniet <colin.geniet@gmail.com>
2018-02-09 18:02:18 +0100

    Fixes syntax errors - WIP
    Fixes a incorrect keywords and unmatched parens/brackets.
    Not in a working state by any mean.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -17,82 +17,81 @@ Chaque bouton doit mener a un état de la state machine
 **/
 
 /** Here we build the finite state machine;
-      
-      Lets say it's
-      
-      main_menu -> option
-      main_menu -> game
-      option -> menu
-      game -> game_menu
-      game_menu -> game
-      game_menu -> main_menu
-      
-      */
-      
-  
+
+Lets say it's
+
+main_menu -> option
+main_menu -> game
+option -> menu
+game -> game_menu
+game_menu -> game
+game_menu -> main_menu
+
+*/
+
+
 
 /*Plus tard; menu = new StateMenu() ...*/
 val menu = new StateScene() {
-      /**
-      
-      A DECOUPLER, VOIR AU DESSUS
-      
-      **/
-      
-      p = Game.fsm
-    
-      fill = Black
-      
-      content = new HBox {
-        padding = Insets(20)
-        children = Seq(
-        
-          new Text
-          {
-            text = "Tynooc"
-            
-            style = "-fx-font-size: 48pt"
-            
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Cyan, DodgerBlue)
-            )
-            
-            effect = new DropShadow {
-              color = DodgerBlue
-              radius = 25
-              spread = 0.25
-            }
-          },
-          
-          new Button {
-            text = "Play"
-            onAction = (event: ActionEvent) =>  { )}
-          },
-          
-          new Button {
-          
-            text = "Quit"
-            onAction = (event: ActionEvent) =>  { stage.close() }
-          }
-        
-        
+  /**
+
+  A DECOUPLER, VOIR AU DESSUS
+
+  **/
+
+  p = Game.fsm
+
+  fill = Black
+
+  content = new HBox {
+    padding = Insets(20)
+    children = Seq(
+
+      new Text
+      {
+        text = "Tynooc"
+
+        style = "-fx-font-size: 48pt"
+
+        fill = new LinearGradient(
+          endX = 0,
+          stops = Stops(Cyan, DodgerBlue)
         )
+
+        effect = new DropShadow {
+          color = DodgerBlue
+          radius = 25
+          spread = 0.25
+        }
+      },
+
+      new Button {
+        text = "Play"
+        onAction = (event: ActionEvent) =>  {}
+      },
+
+      new Button {
+
+        text = "Quit"
+        onAction = (event: ActionEvent) =>  { stage.close() }
       }
-    }
-    
-Game = new Game() /** The fsm is in that **/
+
+
+    )
+  }
+}
+
+game = new Game() /** The fsm is in that **/
 
 object ScalaFXHelloWorld extends JFXApp {
 
   stage = new PrimaryStage {
-  
+
     title = "Tiny Tycoon Tynooc"
-    
+
     state = new FSMScene(Menu.menu)
-    StateScene inherit Scene
+    StateScene extends Scene
     scene = state.current //Faux
-      
-    }
+
   }
 }
--- src/main/scala/statemachine.scala
+++ src/main/scala/statemachine.scala
@@ -1,12 +1,12 @@
 
-class StateScene (p : FSMSCene) inherit Scene
+class StateScene (p : FSMSCene) extends Scene
 {
   private val parent = p
   def goTo(s : StateScene) { p.changeScene(s) }
 }
 
 class FSMScene (init : StateScene) {
-  
+
   var currentState = init
   def changeScene(s: StateScene) {currentState = s}
 }
