892f101884826fd52b7703771b5ea0876c426f67
Colin Geniet <colin.geniet@gmail.com>
2018-02-17 15:06:26 +0100

    Renames directory Scenes to scenes.

---
+++ src/main/scala/scenes/game.scala
@@ -0,0 +1,22 @@
+import scalafx.Includes._
+import scalafx.scene._
+import scalafx.scene.control._
+import scalafx.event._
+import scalafx.scene.layout._
+import scalafx.geometry._
+
+class GameScene(sceneModifier: DisplayStates.Val=>Unit)
+extends DisplayScene(sceneModifier) {
+  private var menuBtn = new Button("Menu")
+  menuBtn.onAction = (event: ActionEvent) => {
+    sceneModifier(DisplayStates.MainMenu)
+  }
+
+  root = new BorderPane(
+    new WorldPane,
+    new MenuPane(menuBtn),
+    new TrainPane,
+    new TownPane,
+    new PlayerPane,
+  )
+}
