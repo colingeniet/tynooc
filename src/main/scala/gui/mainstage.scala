ba3de4f7bf775eae81735ce4ba005dee7dcaa96a
Colin Geniet <colin.geniet@gmail.com>
2018-02-24 12:27:10 +0100

    Revert "Revert "Merge branch 'gui' into dev""
    This reverts commit 3851c22fd3bab08e9e2a4c6790ee270061b4b1ef.
    Revert revert of rerge on a copy on dev to allow merging in gui
    correctly.

---
+++ src/main/scala/gui/mainstage.scala
@@ -0,0 +1,68 @@
+package gui
+
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.application.Platform
+
+import gui.scenes._
+import logic.world.World
+
+/** Main window manager.
+ *
+ *  Handles, displays and switch between menus and game screens.
+ */
+class MainStage extends JFXApp.PrimaryStage {
+  /* Actual scenes displayed. */
+  private var mainMenuScene: MainMenu = new MainMenu(changeScene)
+  private var gameScene: Game = new Game(changeScene)
+  private var optionsScene: Options = new Options(changeScene)
+  /* Stage configuration. */
+  title.value = "Tynooc"
+  scene = mainMenuScene
+  width = 1024
+  height = 720
+
+  /** Callback method that can be used to switch scene.
+   *
+   *  @param newScene the scene to switch to.
+   */
+  def changeScene(newScene: MainStage.States.Val): Unit = {
+    newScene match {
+      case MainStage.States.MainMenu => scene = mainMenuScene
+      case MainStage.States.GameMenu => ()
+      case MainStage.States.Game => scene = gameScene
+      case MainStage.States.Options => scene = optionsScene
+      case MainStage.States.Quit => Platform.exit ()
+    }
+  }
+
+  def world: World = gameScene.world
+
+  def world_=(newWorld: World): Unit = {
+    gameScene.world = newWorld
+  }
+}
+
+/** Main window manager companion object.
+ */
+object MainStage {
+  /** Scenes available to display by the MainStage.
+   */
+  object States {
+    sealed trait Val
+    case object MainMenu extends Val
+    case object GameMenu extends Val
+    case object Game     extends Val
+    case object Options  extends Val
+    case object Quit     extends Val
+  }
+
+  /** The type of a scene manipulated by the MainStage.
+   *
+   *  @param sceneModifier a callback that can be used to switch to
+   *    a different scene.
+   */
+  abstract class Scene(sceneModifier: States.Val=>Unit)
+  extends scalafx.scene.Scene
+}
