888f10425f75526a45c221235c4c6ee7b9ef9154
Karnaj <yoan.geran@hotmail.fr>
2018-02-25 20:07:58 +0100

    Update travel.scala

--- src/main/scala/logic/travel.scala
+++ src/main/scala/logic/travel.scala
@@ -4,6 +4,8 @@ import logic.train._
 import logic.world._
 import logic.town._
 import logic.room._
+import logic.route._
+import logic.player._
 
 object State {
   sealed trait Val
@@ -12,53 +14,53 @@ object State {
   case object ARRIVED extends Val //Passengers are leaving now  
 }
 
-class Travel(val train: Train, private val roads: List[World.Route], 
-             val owner: Player, private val rooms : List[Rooms]) {
+class Travel(val train: Train, private val roads: List[Route], 
+             val owner: Player, private val rooms : List[Room]) {
   
-  if(!owner.owns_train(train)) 
+  if(!owner.owns(train)) 
     throw new IllegalArgumentException("Player doesn’t own the train")
 
   private val distance: Double = (roads.map { _.length }).sum
   private var distanceDone: Double = 0
-  private var remainingRoads: List[World.Route] = roads
-  private var actualRoad: World.Route = remaining_stops.head
-  private val actualTown: World.Town = actual_road.start
+  private var remainingRoads: List[Route] = roads
+  private var actualRoad: Route = remainingRoads.head
+  private var actualTown: Town = actualRoad.start
   private var state: State.Val = State.WAITING
   /* private var actualRoadIndex = 0 */
   
-  def next_step: World.Town = actual_road.end
-  def destination: World.Town = roads.last.end
-  def town: World.Town = actual_town
-  def isDone: Boolean = remaining_roads.isEmpty
+  def next_step: Town = actualRoad.end
+  def destination: Town = roads.last.end
+  def town: Town = actualTown
+  def isDone: Boolean = remainingRoads.isEmpty
   
   def totalRemainingDistance: Double = (remainingRoads.map { _.length }).sum - distanceDone
   def remainingDistance: Double = actualRoad.length - distanceDone 
   def remainingTime: Double = remainingDistance / train.engine.speed 
   
-  def passengerNumber: Int = (rooms.maps { _.passengerNumber}).sum 
+  def passengerNumber: Int = (rooms.map { _.passengerNumber}).sum 
   
-  def isWaiting:  State.Val = state == WAITING
-  def isOnRoad:   State.Val = state == ON_ROAD
-  def is_arrived: State.Val = state == ARRIVED
+  def isWaiting: Boolean = state == State.WAITING
+  def isOnRoad: Boolean = state == State.ON_ROAD
+  def isArrived: Boolean = state == State.ARRIVED
   
-  def availableRooms: List[Rooms] = rooms.filter { _.isAvailable }
+  def availableRooms: List[Room] = rooms.filter { _.isAvailable }
   
   def landPassengers: Unit = {
     rooms.foreach { room => 
-      Game.world.Status.each { status => 
-        city.addResident(room[status.id], status)
-        room.freePlaces(status, actualTown)
+      Game.world.status.foreach { status => 
+        actualTown.addResidents(room.passengers(status.id)(actualTown.id), status)
+        room.freePlaces(room.passengers(status.id)(actualTown.id), actualTown, status)
       }     
     }
   }
   
   def update(dt: Double): Unit = {
-    return if(is_done)
-    
+    if(isDone)
+      return
     state match {
       case State.ON_ROAD => {
-        distanceDone += World.realToVirtualTime(dt) * train.engine.speed
-        if distanceDone >= actualRoad.distance {
+        distanceDone = distanceDone + World.realToVirtualTime(dt) * train.engine.speed
+        if(distanceDone >= actualRoad.length) {
           state = State.ARRIVED
           distanceDone = 0
         }
@@ -72,7 +74,7 @@ class Travel(val train: Train, private val roads: List[World.Route],
       }
       case State.WAITING => {
         state = State.ON_ROAD
-        actual_town = actualRoad.destination
+        actualTown = actualRoad.destination
       }
     }
   } 
