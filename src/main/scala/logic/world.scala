95c6b5d888b77409c52f82bd115d81af698d8c06
Karnaj <yoan.geran@hotmail.fr>
2018-02-25 17:49:47 +0100

    Update world.scala

--- src/main/scala/logic/world.scala
+++ src/main/scala/logic/world.scala
@@ -2,29 +2,42 @@ package logic.world
 
 import logic.graph._
 import logic.game._
-import logic.PNJ._
 import logic.player._
+import logic.route._
+import logic.town._
 
+
+object Status {
+  sealed class Val(val id: Int) {
+    _id += 1
+    def this() { this(_id) } 
+  } 
+  object WAITING extends Status.Val
+  object ON_ROAD extends Status.Val
+  object SETTLED extends Status.Val 
+}
 
 /** World representation
  */
 class World extends Graph {
   
+  var statusNumber = 3
+  var status = List(Status.WAITING, Status.ON_ROAD, Status.SETTLED)
+  
   private var _towns: List[World.Town] = List()
   private var _travels: List[Travel] = List()
-  private var _pnjs: List[PNJ] = List()
   private var _population: Int = 50
   /** The list of towns.
    */
   def towns: List[World.Town] = _towns
 
+  def tryTravel(start:Town, destination:Town, migrantByStatus:Array[Int]):Unit = {
+  
+  }
   /** The list of current travels.
    */
   def travels: List[Travel] = _travels
 
-  /** The list of pnjs.
-   */
-  def pnjs: List[PNJ] = _pnjs
 
   /** The population
    */
@@ -38,93 +51,21 @@ class World extends Graph {
     _towns = newTown :: _towns
   }
 
-  /** Add Pnjs to a Town
-    *
-    * @param p the PNJ to add
-    */
-  def addPNJ(p: PNJ): Unit = {
-    p.town.population += 1
-    _population += p.number
-    _pnjs = p :: _pnjs
-  }
-
   def vertices: List[World.Town] = towns
 
   /** Update the world
   *
   *   @param dt the delta time between two calls.
   */
-  def update(dt: Double) =
+  def update(dt: Double): Unit =
   {
-    pnjs.foreach {p:PNJ => p.update(dt)}
+   
   }
 }
 
 /** World object companion
  */
 object World {
-
-  /** A town in the world.
-   *
-   *  @constructor creates a town in the `World`.
-   *  @param name the town name.
-   *  @param xPos the town x coordonate.
-   *  @param yPos the town y coordonate.
-   */
-  class Town(name: String, xPos: Double, yPos: Double, w: Double)
-  extends Graph.Vertice {
-    /** The town x coordonate in the world. */
-    val x: Double = xPos
-    /** The town y coordonate in the world. */
-    val y: Double = yPos
-    /* The welcoming level of a town, between 0 and 1 */
-    val welcomingLevel: Double = w
-
-    private var _routes: List[Route] = List()
-
-    var population: Int = 0
-
-    /** The list of routes.
-     */
-    def routes: List[Route] = _routes
-
-    /** Adds a route.
-     *
-     *  @param newRoute the route to add.
-     */
-    def addRoute(newRoute: Route): Unit = {
-      _routes = newRoute :: _routes
-    }
-
-    def incidentEdges: List[Route] = routes
-
-    def neighbours: List[Town] = {
-      routes.map { _.destination }
-    }
-
-    def note: Double = {
-      /* Formulas to find */
-      (welcomingLevel*population)/Game.world.population
-    }
-  }
-
-  /** A route starting from this town.
-   *
-   *  @constructor creates a route to another `Town`.
-   *  @param _destination the route destination town.
-   *  @param _length the route length.
-   */
-  class Route(from: Town, to: Town, _length: Double) extends Graph.Edge {
-    val start: Town = from
-    val end: Town = to
-    val weight: Double = _length
-
-    /** The route destination. */
-    def destination: Town = end
-
-    /** The route length. */
-    def length: Double = weight
-  }
 
   def real_to_virtual_time(t: Double) : Double = {
     return 50*t
