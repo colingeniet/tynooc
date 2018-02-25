6c92be85fa47e26fd98dadf3dd2b270ec793e037
Karnaj <yoan.geran@hotmail.fr>
2018-02-25 19:59:53 +0100

    Update room.scala

--- src/main/scala/logic/room.scala
+++ src/main/scala/logic/room.scala
@@ -2,8 +2,10 @@ package logic.room
 
 import logic.train._
 import logic.player._
-import logic.world._
 import logic.travel._
+import logic.game._
+import logic.world._
+
 
 final case class CantBuy(private val message: String = "", 
                          private val cause: Throwable = None.orNull)
@@ -12,31 +14,34 @@ final case class CantBuy(private val message: String = "",
 final case class CantFree(private val message: String = "", 
                          private val cause: Throwable = None.orNull)
                          extends Exception(message, cause)
-                         
+
+
 class Room(val price: Int, val travel: Travel, val carriage: Carriage) {
 
   private var _passengers: Array[Array[Int]] = Array.ofDim(Game.world.statusNumber,
                                                            Game.world.townNumber)
   
-  def passengers: Array[Array[[Int]] = _passengers
+  def passengers: Array[Array[Int]] = _passengers
   def passengerNumber: Int = (passengers.map { _.sum}).sum
   def capacity: Int = carriage.capacity
   def isAvailable: Boolean = passengerNumber < capacity
   def availablePlaces: Int = capacity - passengerNumber
   def comfort: Double = carriage.comfort
   
-  def statusToTownNumber(destination: Town, status: Status.val): Int = {
-    passengers[status.id][destination]
+  def statusToTownNumber(destination: Town, status: Status.Val): Int = {
+    passengers(status.id)(destination.id)
   }
   
-  def freePlaces(nb: Int, destination:Town, status: Status.val): Unit = {
-    throw new CantFree if nb > passengers[status.id][destination]
-    passengers[status.id][destination] -= nb 
+  def freePlaces(nb: Int, destination: Town, status: Status.Val): Unit = {
+    if(nb > passengers(status.id)(destination.id))
+      throw new CantFree 
+    passengers(status.id)(destination.id) -= nb 
   } 
   
-  def takePlaces(nb: Int, destination: Town, status: Status.val):Unit = {
-    throw new CantBuy if nb > availablePlaces
-    passengers[status.id][destination] += nb
+  def takePlaces(nb: Int, destination: Town, status: Status.Val):Unit = {
+    if(nb > availablePlaces)
+      throw new CantBuy 
+    passengers(status.id)(destination.id) += nb
     travel.owner.addMoney(price * nb)
   }
 }
