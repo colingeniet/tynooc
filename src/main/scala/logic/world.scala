0eadb11626d3ee451823f891ffb4323e1724612a
Karnaj <yoan.geran@hotmail.fr>
2018-02-26 12:00:36 +0100

    Update world.scala

--- src/main/scala/logic/world.scala
+++ src/main/scala/logic/world.scala
@@ -1,16 +1,17 @@
 package logic.world
 
 import logic.graph._
-import logic.game._
 import logic.town._
 import logic.travel._
 
 object Status {
   var _id = 0
+
   sealed class Val(val id: Int) {
     _id += 1
     def this() { this(_id) } 
-  } 
+  }
+
   object RICH extends Status.Val
   object POOR extends Status.Val
   object WELL extends Status.Val 
@@ -28,13 +29,22 @@ class World extends Graph {
   def towns: List[Town] = _towns
   def vertices: List[Town] = towns
   def travels: List[Travel] = _travels
-  def population: Int = (towns.map { _.population }).sum
+  def population: Int = towns.foldLeft[Int](0) { _ + _.population }
   
-  def addTown(town: Town): Unit = { _towns = town :: _towns }
-  
+  def addTown(town: Town): Unit = { _towns = town :: _towns; townNumber += 1}
+
+  def addTown(name: String, x: Double, y: Double, welcomingLevel: Double): Unit = {
+    _towns = new Town(name, x, y, welcomingLevel) :: _towns
+    townNumber += 1
+  }
+
   def tryTravel(start:Town, destination:Town, migrantByStatus:Array[Int]):Unit = {}
 
   def update(dt: Double): Unit = { }
+
+  override def toString: String = {
+    towns.foldLeft[String]("") { (d, t) => d + s"$t\n" }
+  }
 }
 
 object World {
