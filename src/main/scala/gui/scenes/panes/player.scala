0c855761bc4c6ac742f4796f822d93dacd14598f
Colin Geniet <colin.geniet@gmail.com>
2018-02-26 18:17:30 +0100

    Improves player pane.

--- src/main/scala/gui/scenes/panes/player.scala
+++ src/main/scala/gui/scenes/panes/player.scala
@@ -16,6 +16,12 @@ class PlayerInfo(
   detailEngine: Engine => Unit,
   detailCarriage: Carriage => Unit
 ) extends DrawableVBox {
+  // general info
+  private var money: Label = new Label()
+
+  private var sep1: Separator = new Separator()
+
+  // list menu
   private var listGroup: ToggleGroup = new ToggleGroup
 
   private var trainsButton: RadioButton = new RadioButton("trains") {
@@ -39,37 +45,44 @@ class PlayerInfo(
   }
   listGroup.toggles.add(carriagesButton)
 
-  private var sep: Separator = new Separator()
-  trainsButton.fire()
+  private var sep2: Separator = new Separator()
 
+  private var list: ScrollPane = new ScrollPane()
+
+  // display content
+  trainsButton.fire()
+  draw()
+
+  // lists
   private def displayTrains(): Unit = {
-    children = List(
-      trainsButton,
-      enginesButton,
-      carriagesButton,
-      sep,
-      new TrainList(player.trains, detailTrain)
-    )
+    list = new TrainList(player.trains, detailTrain)
+    setChildren()
   }
 
   private def displayEngines(): Unit = {
-    children = List(
-      trainsButton,
-      enginesButton,
-      carriagesButton,
-      sep,
-      new EngineList(player.engines, detailEngine)
-    )
+    list = new EngineList(player.engines, detailEngine)
+    setChildren()
   }
 
   private def displayCarriages(): Unit = {
+    list = new CarriageList(player.carriages, detailCarriage)
+    setChildren()
+  }
+
+  private def setChildren(): Unit = {
     children = List(
+      money,
+      sep1,
       trainsButton,
       enginesButton,
       carriagesButton,
-      sep,
-      new CarriageList(player.carriages, detailCarriage)
+      sep2,
+      list
     )
+  }
+
+  override def draw(): Unit = {
+    money.text = player.money.toInt + "$"
   }
 }
 
