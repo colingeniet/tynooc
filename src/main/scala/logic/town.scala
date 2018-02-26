e361a0b031ac5365cc930a31019e699206c293c5
Karnaj <yoan.geran@hotmail.fr>
2018-02-26 12:01:43 +0100

    Update town.scala

--- src/main/scala/logic/town.scala
+++ src/main/scala/logic/town.scala
@@ -1,21 +1,26 @@
 package logic.town
 
 import logic.graph._
-import logic.routes._
+import logic.route._
 import logic.game._
 import logic.world._
 
-class Town(val name: String, val x: Double, val y: Double, val id: Int,  
+object TownId {
+  private var id = 0
+  def nextId: Int = { id += 1; id }
+}
+
+class Town(val name: String, val x: Double, val y: Double,
            val welcomingLevel: Double) extends Graph.Vertice {
-           
+
+  val id = TownId.nextId
   private var residents: Array[Int] = new Array(Game.world.statusNumber)
   private var _routes: List[Route] = List()
-  
+
   def routes: List[Route] = _routes
   def incidentEdges: List[Route] = routes
-  def addRoute(route: Route): Unit = _routes = route :: _routes
   def neighbours: List[Town] = routes.map { _.end }
-  def note: Double = welcomingLevel * population / Game.world.population
+  def note: Double = welcomingLevel * (1 + population) / (1 + Game.world.population) /* + 1 to avoid division by zero */
   def population: Int = residents.sum
   
   def addResidents(nb: Int, status: Status.Val): Unit = residents(status.id) += nb
@@ -27,7 +32,17 @@ class Town(val name: String, val x: Double, val y: Double, val id: Int,
   }
   
   def generateMigrant(to: Town):Int = Math.max(0, population * (to.note - note)).toInt
-  
+
+
+  def addRoute(route: Route): Unit = {
+    /* Check if route.start == this */
+    _routes = route :: _routes
+  }
+
+  def addRoute(end: Town, length: Double): Unit = {
+    _routes = (new Route(this, end, length)) :: _routes
+  }
+
   def update(dt: Double): Unit = {
     val p = population
     val possibleDestinations = neighbours.sortBy { _.note }
@@ -36,5 +51,9 @@ class Town(val name: String, val x: Double, val y: Double, val id: Int,
       val byStatus = residents.map { r => (migrantNumber * r / p).toInt }
       Game.world.tryTravel(this, destination, byStatus)
     }
+  }
+
+  override def toString: String = {
+    s"$name: $population residents, $welcomingLevel welcoming level, $note note at ($x, $y)."
   }
 }
