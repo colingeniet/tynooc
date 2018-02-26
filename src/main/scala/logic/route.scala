76c1495223cf602a1d354824cfd6eac274ef0749
Karnaj <yoan.geran@hotmail.fr>
2018-02-26 12:04:20 +0100

    Update route.scala

--- src/main/scala/logic/route.scala
+++ src/main/scala/logic/route.scala
@@ -1,5 +1,8 @@
 package logic.route
 
+import logic.graph._
+import logic.town._
+
 class Route(val start: Town, val end: Town, val weight: Double) extends Graph.Edge {
   
   def destination: Town = end
