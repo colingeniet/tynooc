cb82a55b9c7cfbc3ec68d6d0ebf8a02e07cb682b
Karnaj <yoan.geran@hotmail.fr>
2018-02-28 12:13:46 +0100

    fabricTown is now a player attribute.

--- src/main/scala/logic/player.scala
+++ src/main/scala/logic/player.scala
@@ -18,7 +18,7 @@ object PriceSimulation {
 }
 
 
-class Player() {
+class Player(val fabricTown: Town = Game.world.towns(0)) {
   var trains: List[Train] = List()
   var carriages: List[Carriage] = List()
   var engines: List[Engine] = List()
@@ -48,7 +48,7 @@ class Player() {
     if (!engines.contains(engine)) {
       throw new IllegalArgumentException("Player doesn’t own the engine")
     }
-    val train = new Train(engine, List())
+    val train = new Train(engine, List(), fabricTown)
     trains = train :: trains
     engines = engines diff List(engine)
     train
