9e64d85649ddc82ba52fd4d01fc80bd7245c8ce4
Karnaj <yoan.geran@hotmail.fr>
2018-03-01 14:23:17 +0100

    Fix `consumption` method.

--- src/main/scala/logic/train.scala
+++ src/main/scala/logic/train.scala
@@ -127,7 +127,7 @@ class Train (
     carriages.foldLeft[Double](engine.model.weight)(_ + _.model.weight)
   }
 
-  def consumption(distance: Double): Double = engine.consumption * distance
+  def consumption(distance: Double): Double = engine.model.consumption * distance
   
   def tooHeavy: Boolean = weight > engine.model.power
 
