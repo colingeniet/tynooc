dde9262805f9c4c5022e5a72eb0ea7b7241f5bfe
Karnaj <yoan.geran@hotmail.fr>
2018-03-01 17:51:54 +0100

    Adds the state of the noute (between 0 and 1).

--- src/main/scala/logic/route.scala
+++ src/main/scala/logic/route.scala
@@ -3,4 +3,4 @@ package logic.route
 import logic.graph._
 import logic.town._
 
-class Route(val start: Town, val end: Town, val length: Double)
+class Route(val start: Town, val end: Town, val length: Double, val state: Double)
