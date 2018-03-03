17fe251919bf915f4935f140cdc2792602330a0e
Colin Geniet <colin.geniet@gmail.com>
2018-03-03 15:54:06 +0100

    Adds documentation to `Game` object.

--- src/main/scala/logic/game.scala
+++ src/main/scala/logic/game.scala
@@ -3,17 +3,24 @@ package logic.game
 import logic.player._
 import logic.world._
 
+/** Game logic main object. */
 object Game {
+  /* Time of last update. Used to control simulation rate. */
   private var last: Double = System.currentTimeMillis()
+
   var world: World = new World()
   var time: Double = 0
 
+  /* Simulation rate control. */
   var paused: Boolean = false
   var timeAcceleration: Double = 1
 
+  /** Advance simulation.
+   */
   def update(): Unit = {
     val a: Double = System.currentTimeMillis()
     if (!paused) {
+      // in game time passed
       val dt: Double = timeAcceleration * realToVirtualTime(a - last)
       logic(dt)
       time += dt
@@ -21,11 +28,16 @@ object Game {
     last = a
   }
 
+  /** Game logic simulation step.
+   *
+   *  @param dt in game time passed since last step.
+   */
   def logic(dt: Double): Unit = {
     //Update Cities
     world.update(dt)
   }
 
+  /** Reinitialize game state. */
   def reset(): Unit = {
     world = new World()
     time = 0
@@ -37,8 +49,20 @@ object Game {
   // 4 sec (real time) = 1 hours (game time)
   def realToVirtualTime(t: Double): Double = t / 4000
 
+  /** Convert a time as a double to its string representation.
+   *
+   *  The double value is interpreted as hours.
+   *  Format : <Hours>h<Min>
+   */
   def timeToHourString(t: Double): String =
     f"${t.floor}%02.0fh${t * 60 % 60}%02.0f"
+
+  /** Convert a date as a double to its string representation.
+   *
+   *  The double value is interpreted as hours.
+   *  Days numbering start from 1.
+   *  Format : <Day> : <Hours>h<Min>
+   */
   def timeToDateString(t: Double): String =
     f"day ${(t.toInt / 24 + 1)}%d : " + timeToHourString(t % 24)
 }
