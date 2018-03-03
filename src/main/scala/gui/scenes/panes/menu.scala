c187153e3e6b0139a5159d9f5cae4b10c9d4956b
Colin Geniet <colin.geniet@gmail.com>
2018-03-03 15:23:38 +0100

    Increases time acceleration.

--- src/main/scala/gui/scenes/panes/menu.scala
+++ src/main/scala/gui/scenes/panes/menu.scala
@@ -40,11 +40,11 @@ extends BorderPane with Drawable {
   }
   val buttonX2 = new ToggleButton {
     graphic = new ImageView {image = new Image(this, "/icons/time2.png")}
-    onAction = (event: ActionEvent) => Game.timeAcceleration = 2
+    onAction = (event: ActionEvent) => Game.timeAcceleration = 3
   }
   val buttonX4 = new ToggleButton {
     graphic = new ImageView {image = new Image(this, "/icons/time3.png")}
-    onAction = (event: ActionEvent) => Game.timeAcceleration = 4
+    onAction = (event: ActionEvent) => Game.timeAcceleration = 8
   }
   timeGroup.toggles.addAll(buttonX1, buttonX2, buttonX4)
 
