e1680fa65499f3a0f73c485b73665d89f6da004f
Colin Geniet <colin.geniet@gmail.com>
2018-03-05 14:40:24 +0100

    Fix time control buttons.
    Use radio buttons instead of toggle buttons to prevent deselection.

--- src/main/scala/gui/scenes/panes/menu.scala
+++ src/main/scala/gui/scenes/panes/menu.scala
@@ -38,18 +38,24 @@ extends BorderPane with Drawable {
   }
 
   val timeGroup: ToggleGroup = new ToggleGroup()
-  val buttonX1 = new ToggleButton {
+  val buttonX1 = new RadioButton {
     graphic = new ImageView {image = new Image(this, "/icons/time1.png")}
     onAction = (event: ActionEvent) => Game.timeAcceleration = 1
+    styleClass.remove("radio-button")
+    styleClass.add("toggle-button")
     fire()
   }
   val buttonX2 = new ToggleButton {
     graphic = new ImageView {image = new Image(this, "/icons/time2.png")}
     onAction = (event: ActionEvent) => Game.timeAcceleration = 2.5
+    styleClass.remove("radio-button")
+    styleClass.add("toggle-button")
   }
   val buttonX3 = new ToggleButton {
     graphic = new ImageView {image = new Image(this, "/icons/time3.png")}
     onAction = (event: ActionEvent) => Game.timeAcceleration = 6
+    styleClass.remove("radio-button")
+    styleClass.add("toggle-button")
   }
   timeGroup.toggles.addAll(buttonX1, buttonX2, buttonX3)
 
