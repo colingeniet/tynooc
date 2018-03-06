d7106a93208cc776a25674a87e9a47e666139a39
Colin Geniet <colin.geniet@gmail.com>
2018-03-06 22:19:38 +0100

    Fix formatter

--- src/main/scala/formatter/formatter.scala
+++ src/main/scala/formatter/formatter.scala
@@ -2,17 +2,15 @@ package formatter
 
 import java.util.{Currency, Locale}
 
-/** An object which permitds to format money displaying. */ 
+/** An object which permitds to format money displaying. */
 object MoneyFormatter {
-  private val formatter = java.text.NumberFormat.getCurrencyInstance
-  private val fr = Currency.getInstance(new Locale("fr", "FR"))
-  formatter.setCurrency(fr)
-  
-  /** Formats <code>money</code> using euros.  
+  private val formatter = new java.text.DecimalFormat("€#,###.##;€-#,###.##")
+
+  /** Formats <code>money</code> using euros.
     *
     * @param money The money amount to format.
     */
   def format(money: Double): String = {
     formatter.format(money)
-  }  
+  }
 }
