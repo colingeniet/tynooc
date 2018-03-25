65821d9912ffe11ad8542f593f0501a2c89ccff2
Colin Geniet <colin.geniet@gmail.com>
2018-03-25 16:38:15 +0200

    Create travel script classes.

---
+++ src/main/scala/logic/travel/travel_script.scala
@@ -0,0 +1,42 @@
+package logic.travel
+
+import logic.town._
+import logic.game._
+import logic.company._
+import logic.vehicle.train._
+
+
+trait TravelInstruction {
+  def execute(onCompleted: () => Unit = () => ()): Unit
+}
+
+case class TravelTo(company: Company, train: Train, town: Town)
+extends TravelInstruction {
+  def execute(onCompleted: () => Unit): Unit = {
+    company.launchTravel(train, town, onCompleted)
+  }
+}
+
+case class Wait(delay: Double)
+extends TravelInstruction {
+  def execute(onCompleted: () => Unit): Unit = {
+    Game.delayAction(delay, onCompleted)
+  }
+}
+
+case class DoTimes(times: Int, instr: List[TravelInstruction])
+extends TravelInstruction {
+  def execute(onCompleted: () => Unit): Unit = executePartial(onCompleted)
+
+  private def executePartial(
+    onCompleted: () => Unit,
+    timesLeft: Int = times,
+    instrLeft: List[TravelInstruction] = instr): Unit = {
+    if(instrLeft.isEmpty) {
+      if(timesLeft > 0) executePartial(onCompleted, timesLeft-1)
+      else onCompleted()
+    } else {
+      instrLeft.head.execute(() => executePartial(onCompleted, timesLeft, instrLeft.tail))
+    }
+  }
+}
