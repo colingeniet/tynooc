a268967b3cd7e227a7e506008bf8f4c72a23e060
Colin Geniet <colin.geniet@gmail.com>
2018-04-06 17:34:28 +0200

    Create airport and port models.

--- src/main/scala/logic/facility/station.scala
+++ src/main/scala/logic/facility/station.scala
@@ -1,1 +1,37 @@
 package logic.facility
+
+import logic.model._
+import logic.town._
+import logic.vehicle._
+
+
+trait StationModel extends FacilityModel {
+  val size: Int
+}
+
+trait Station extends Facility {
+  def accepts(vehicle: Vehicle): Boolean
+}
+
+abstract class StationFromModel[Model <: StationModel] (
+  model: Model,
+  town: Town)
+extends FacilityFromModel[Model](model, town) with Station
+
+
+
+class AirportModel(
+  val name: String,
+  val price: Double,
+  val upgrades: List[String],
+  val size: Int,
+  val runwayLength: Double)
+extends StationModel
+
+class PortModel(
+  val name: String,
+  val price: Double,
+  val upgrades: List[String],
+  val size: Int,
+  val beamClearance: Double)
+extends StationModel
