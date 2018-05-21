57aaf19a3bb0dc596769f0c6ebab63cc353e51d0
Colin Geniet <colin.geniet@gmail.com>
2018-05-21 11:21:20 +0200

    Adds price in goods summary table

--- src/main/scala/gui/scenes/panes/townstats.scala
+++ src/main/scala/gui/scenes/panes/townstats.scala
@@ -10,6 +10,7 @@ import scalafx.beans.property._
 import logic.world._
 import logic.town._
 import logic.good._
+import formatter._
 
 class TownStats(towns: List[Town]) extends TableView(ObservableBuffer(towns)) {
   val nameColumn = new TableColumn[Town, String]("Town") {
@@ -23,8 +24,9 @@ class TownStats(towns: List[Town]) extends TableView(ObservableBuffer(towns)) {
       cellValueFactory = {
         c: TableColumn.CellDataFeatures[Town, String] =>
           createStringBinding(
-            () => f"${c.value.goods(g).toDouble}%.2f",
-            c.value.goods(g))
+            () => f"${c.value.goods(g)()}%.0f (${MoneyFormatter.format(c.value.goods_prices(g)())})",
+            c.value.goods(g),
+            c.value.goods_prices(g))
       }
     }.delegate
   }
